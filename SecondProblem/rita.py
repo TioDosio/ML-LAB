900d3ea8fd5e84968f8a9d840a9d1a594ce8e9c5
André Teodósio <andre.teodosio@tecnico.ulisboa.pt>
2023-10-06 12:34:27 +0100

    simplifiquei o que fizeste

--- SecondProblem/rita.py
+++ SecondProblem/rita.py
@@ -3,8 +3,9 @@ from sklearn.linear_model import RANSACRegressor, LinearRegression, Lasso, Ridge
 import random
 import warnings
 import itertools
-import sys 
+import sys
 from sklearn.model_selection import KFold
+
 
 def main():
     warnings.filterwarnings("ignore")
@@ -15,8 +16,8 @@ def main():
 
     index_list = list(range(len(X_train)))
     random.shuffle(index_list)
-    #inlier_range = 0.73
-    inlier_range = np.arange(0.72,0.725,0.00001)
+    # inlier_range = 0.73
+    inlier_range = np.arange(0.72, 0.725, 0.00001)
     best_inlier_count = 0
     inlier_model = None
     combinations = list(itertools.combinations(range(len(X_train)), 2))
@@ -37,7 +38,7 @@ def main():
             linear.fit(x, y)
             predictions = linear.predict(X_train)
             distances = np.abs(predictions - Y_train)
-            inlier_count = np.sum(distances < i) 
+            inlier_count = np.sum(distances < i)
             if inlier_count > best_inlier_count:
                 outlier_indices = np.where(distances >= i)[0]
                 inlier_indices = np.where(distances < i)[0]
@@ -50,37 +51,36 @@ def main():
         inlier_y = Y_train[inlier_indices]
         outlier_model = LinearRegression()
         outlier_model.fit(outlier_x, outlier_y)
-        sse_geral = 0
-        error=0
-        aux=0
-        aux2=0
+        error = 0
+        aux = 0
+        aux2 = 0
         for j in range(len(X_train)):
             error_model_1 = (inlier_model.predict([X_train[j]]) - Y_train[j]) ** 2
             error_model_2 = (outlier_model.predict([X_train[j]]) - Y_train[j]) ** 2
 
             if error_model_1 < error_model_2:
                 error += error_model_1
-                aux+=1
+                aux += 1
             else:
                 error += error_model_2
-                aux2+=1
+                aux2 += 1
 
-        sse_geral = error 
-        if sse_geral < sse_baseline:
-            sse_baseline = sse_geral
+        if error < sse_baseline:
+            sse_baseline = error
             best_parameter = i
- 
-    print("melhor parametro ",best_parameter)
+
+    print("melhor parametro ", best_parameter)
     print("Pontos model 1", len(inlier_indices))
     print("Pontos model 2", len(outlier_indices))
     print("sse ", sse_baseline)
     print(f"aux1 {aux}, aux2 {aux2}")
-    
+
     col1 = inlier_model.predict(X_test)
     col2 = outlier_model.predict(X_test)
     np.save('col1', col1)
     np.save('col2', col2)
     np.save('output_rita', np.column_stack((col1, col2)))
- 
+
+
 if __name__ == "__main__":
     main()
