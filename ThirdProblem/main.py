521400243b59ae559ea0b247ec8ccdd3d0b2af65
SparklingRita <rcordeiromendes@gmail.com>
2023-10-14 20:12:32 +0100

    grafico loss/epochs looking sexy

--- ThirdProblem/main.py
+++ ThirdProblem/main.py
@@ -1,32 +1,35 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+from keras.utils import to_categorical
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+lr = 0.0001 #learning rate
 X_train = np.load('Xtrain_Classification1.npy')
 Y_train = np.load('ytrain_Classification1.npy')
 X_test = np.load('Xtest_Classification1.npy')
 
-x = np.reshape(X_train, (-1,28,28,3))
-i=0
-print(x.shape)
-while i < 10:
-    plt.imshow(x[i])
-    plt.show()
-    i+=1
+X_train_scaled = X_train/255
 
+x_train, x_val, y_train, y_val = train_test_split(X_train_scaled, Y_train, test_size=0.33, shuffle= True)
+model = Sequential()
+model.add(Dense(8, input_dim=28 * 28 * 3, activation='relu'))
+model.add(Dense(4, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+adam = Adam(learning_rate=lr)
 
-""" multi-layer perceptor example
-from keras.models import Squencial # -> to add layers sequencialy
+model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'] )
+history = model.fit(x=x_train, y=y_train, batch_size=64, epochs=50, validation_data=(x_val, y_val))
 
-train_img = (X).astype('float32')/255
-255 -> maximum pixel intensity mas podemos normalizar como quisermos
-
-train_labels?leras.utils-to_categoriacal()
-
-model_MLP=Sequential()
-model_MLP.add(Dense(16, activation = Relu, input_dim = 28*28*3))
-
-adam = keras.optimizers.Adam(learning_rate=lr)
-model_MLP.compile(optimizer = adam, loss='categorical_crossentropy', metrics=['accuracy'])
-
-results_MLP = np.argmax(model_MLP.predict(test_images),1)
-"""
+plt.figure(figsize=(10, 6))
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
