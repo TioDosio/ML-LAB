81b6bb9e120ca258efd5c1971df677c53f74ad5e
SparklingRita <rcordeiromendes@gmail.com>
2023-10-15 18:21:20 +0100

    tentativa de smote

--- ThirdProblem/main.py
+++ ThirdProblem/main.py
@@ -1,30 +1,41 @@
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense
 from keras.optimizers import Adam
-from keras.utils import to_categorical
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.utils.class_weight import compute_class_weight
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
-lr = 0.0001 #learning rate
+from sklearn.metrics import f1_score, balanced_accuracy_score
+from imblearn.over_sampling import SMOTE
+import tensorflow as tf
+lr = 0.0001  # Learning rate
 X_train = np.load('Xtrain_Classification1.npy')
 Y_train = np.load('ytrain_Classification1.npy')
 X_test = np.load('Xtest_Classification1.npy')
 
-X_train_scaled = X_train/255
+X_train_scaled = X_train / 255
+x_train, x_val, y_train, y_val = train_test_split(X_train_scaled, Y_train, test_size=0.33, shuffle=True)
 
-x_train, x_val, y_train, y_val = train_test_split(X_train_scaled, Y_train, test_size=0.33, shuffle= True)
 model = Sequential()
-model.add(Dense(8, input_dim=28 * 28 * 3, activation='relu'))
+model.add(Dense(8, activation='relu'))
 model.add(Dense(4, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
+
 adam = Adam(learning_rate=lr)
 
-model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'] )
-history = model.fit(x=x_train, y=y_train, batch_size=64, epochs=50, validation_data=(x_val, y_val))
+smote = SMOTE(sampling_strategy='auto', k_neighbors=5, random_state=42)  # Added random_state for reproducibility
+x_train_reshaped, y_train_reshaped = smote.fit_resample(x_train, y_train)
+
+model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True) # early stopping
+
+history = model.fit(x_train_reshaped, y_train_reshaped, batch_size=64, epochs=200, validation_data=(x_val, y_val),callbacks=[callback])
+
+predict = model.predict(x_train)
+predict_val = model.predict(x_val)
+
+print(f'F1-Score = {f1_score(y_train, np.round(predict))}')
+print(f"Balanced Acc = {balanced_accuracy_score(y_train, np.round(predict))}")
 
 plt.figure(figsize=(10, 6))
 plt.plot(history.history['loss'], label='Training Loss')
